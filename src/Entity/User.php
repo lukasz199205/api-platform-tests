27c9a8fa62c72fdc4dbd35fac6dd57888c058a95
Lukasz <lukasz199205@gmail.com>
2022-02-04 15:40:54 +0100

    __toString() method added

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -35,6 +35,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $password;
 
+    public function __toString()
+    {
+        return $this->email;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
